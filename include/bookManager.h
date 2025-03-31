b4f914c19a7a53d51169b758573f9633488319ce
Julius <maxi-hatschi@web.de>
2025-03-31 18:04:56 +0200

    for now final fixes

--- include/bookManager.h
+++ include/bookManager.h
@@ -12,7 +12,6 @@ std::exception* parseBook(std::vector<std::string>& line, struct book& thisBook)
 std::exception* unparseBook(struct book& thisBook, std::string& line);
 std::exception* removeBook(std::vector<struct book>& books, struct book& delBook);
 std::exception* findBook(std::vector<struct book> &books, std::vector<struct book> &resultBooks, std::string query);
-std::exception* findBook(std::vector<struct book>& books);
 std::exception* sortByDate(std::vector<struct book> &books, bool ascending);
 std::exception* sortByAuthor(std::vector<struct book>& books, bool ascending);
 std::exception* sortByTitle(std::vector<struct book>& books, bool ascending);
